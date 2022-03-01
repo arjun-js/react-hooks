3891e61948675dbcd8fa6df59a7f011f5cab9f20
Arjun <Arjun>
2022-03-02 00:34:31 +0530

    complete hoooks

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
 
 function Ingredients() {
+  const [ingredientList, setIngredientList] = useState([]);
+  const [isLoading, setLoader] = useState(false);
+  const [error, setError] = useState();
+  const addIngredientHandler = (ingredient) => {
+    setLoader(true);
+    fetch(
+      'https://practicing-react-a7ede-default-rtdb.firebaseio.com/ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((response) => {
+        setLoader(false);
+        return response.json();
+      })
+      .then((responseData) => {
+        setIngredientList((prevState) => [
+          ...ingredientList,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const onFilterChangeHandler = useCallback((filteredList=>{
+    setIngredientList(filteredList);
+  }), [setIngredientList]);
+
+  const onRemoveHandler = (id)=>{
+    setLoader(true);
+    fetch(
+      `https://practicing-react-a7ede-default-rtdb.firebaseio.com/ingredients/${id}.json`,
+      {
+        method: 'DELETE',
+      }
+    ).then(response=>{
+      setLoader(false);
+      setIngredientList(prevList=>prevList.filter(ing=>ing.id!==id))
+    }).catch(error=>{
+      setError(error.message);
+    })
+  }
+
+  const onModalClose= ()=>{
+    setError();
+    setLoader(false);
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
 
       <section>
-        <Search />
+        <Search onFilterChangeHandler={onFilterChangeHandler} />
         {/* Need to add list here! */}
       </section>
+      <IngredientList ingredients={ingredientList} onRemoveItem={onRemoveHandler} />
+      {error && <ErrorModal onClose={onModalClose}>{error}</ErrorModal>}
     </div>
   );
 }
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+
+  const {onFilterChangeHandler} = props;
+
+const [enteredText, setEnteredText] = useState('');
+
+const inputRef = useRef();
+
+useEffect(()=>{
+  const timer = setTimeout(()=>{
+    if(enteredText === inputRef.current.value){
+      let query = enteredText.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredText}"`;
+      fetch(
+        'https://practicing-react-a7ede-default-rtdb.firebaseio.com/ingredients.json' + query
+      )
+        .then((responseData) => responseData.json())
+        .then((data) => {
+          const ingredientsData = [];
+    
+          for(const key in data){
+            ingredientsData.push({
+              id: key,
+              title: data[key].title,
+              amount: data[key].amount
+            });
+          }
+       onFilterChangeHandler(ingredientsData);
+        });
+    }
+
+  }, 1000);
+
+  return ()=>{
+    clearTimeout(timer);
+  }
+ 
+}, [enteredText, onFilterChangeHandler, inputRef]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input 
+          value={enteredText}
+          ref={inputRef}
+          onChange={(event)=>{
+            setEnteredText(event.target.value)
+          }}
+          type="text" />
         </div>
       </Card>
     </section>
